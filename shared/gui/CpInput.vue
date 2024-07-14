ec7d8394ea4ba55bfb83c616cb1cf5eca5226012
Никита Павленко <nick.pavlenko1998@gmail.com>
2024-07-14 13:05:14 -0700

    feat(TAZ-110): Created Input Element
    - Added blank component for select
    - Added basic realisation of Input component

---
+++ shared/gui/CpInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      :type="inputType"
+      @focus="$emit('focus', $event)"
+      @blur="$emit('blur', $event)"
+      @input="inputHandler"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+type Props = {
+  inputType: string;
+  modelValue: string;
+};
+
+type Events = {
+  (event: 'blur' | 'focus', eventData: FocusEvent): void;
+  (event: 'input' | 'update:modelValue', eventData: Event): void;
+};
+
+defineProps<Props>();
+const emit = defineEmits<Events>();
+
+const inputHandler = (event: Event) => {
+  emit('input', event);
+  emit('update:modelValue', event);
+};
+</script>
