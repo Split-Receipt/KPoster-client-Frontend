ab66548aebca57a953f5dfce12af3b9b151a98b3
unknown <Romangergisevich@gmail.com>
2024-08-12 17:03:12 +0300

    TAZ-135 clean test variables

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -47,8 +47,6 @@
 import CpDropDown from '@shared/gui/CpDropDown.vue';
 import axios from 'axios';
 
-const testQWE = ref<string>('');
-
 const categoriesToFilter: string[] = [];
 const citiesToFilter: string[] = [];
 const { availableLocales, locale, setLocale } = useI18n();
